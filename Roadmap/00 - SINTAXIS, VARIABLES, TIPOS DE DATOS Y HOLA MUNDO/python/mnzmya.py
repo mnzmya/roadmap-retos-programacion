f47e933e6ddd70d03bb587ba9a40f9d3eb7fa7ab
mnzmya <munozmya10@gmail.com>
2025-07-10 20:42:08 -0700

    mnzmya.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/mnzmya.py
@@ -0,0 +1,13 @@
+#https://www.python.org
+
+#comentario en una sola linea
+
+"""
+la triple comilla doble al inicio y al final es la otra forma para crear comentarios en python, 
+pero con esta froma podemos escribir el comentario en varias lineas 
+"""
+
+name = Mya
+Edad = 14 
+
+FINAL_PAY = 100
